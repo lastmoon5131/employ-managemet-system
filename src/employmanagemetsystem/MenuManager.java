e14a182c30d0335d2b2012bfc5c08badfbf092f6
lastmoon5131 <lastmoon5131@gmail.com>
2023-03-15 16:27:01 +0900

    removed hello.java and added MenuManager.java

---
+++ src/employmanagemetsystem/MenuManager.java
@@ -0,0 +1,13 @@
+package employmanagemetsystem;
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Employee");
+		System.out.println("2. Delete Employee");
+		System.out.println("3. Edit Employee");
+		System.out.println("4. View Employee");
+		System.out.println("5. Exit");
+
+	}
+}
